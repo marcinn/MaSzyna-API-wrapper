e6bcabdcbf0d96f1758a72fba9759c5239fe83cf
Marcin Nowak <marcin.j.nowak@gmail.com>
2024-12-09 13:07:29 +0100

    Add LogSystem
    * decouples logging from TrainSystem
    * adds DEBUG macro helper

---
+++ src/core/LogSystem.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/array.hpp>
+#include <godot_cpp/variant/string.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <map>
+
+#ifdef DEBUG_ENABLED
+#if defined(_MSC_VER)
+// MSVC doesn't require special handling.
+#define DEBUG(msg, ...)                                                                                                \
+    UtilityFunctions::print_rich("[color=orange](debug) " + vformat(String(msg), Array::make(__VA_ARGS__)) + "[/color]")
+#elif defined(__cplusplus) && __cplusplus >= 202002L
+// C++20 with __VA_OPT__
+#define DEBUG(msg, ...)                                                                                                \
+    UtilityFunctions::print_rich(                                                                                      \
+            "[color=orange](debug) " + vformat(String(msg) __VA_OPT__(, ) Array::make(__VA_ARGS__)) + "[/color]")
+#else
+// Use a workaround for C++11 to C++17
+#define DEBUG(msg, ...)                                                                                                \
+    UtilityFunctions::print_rich("[color=orange](debug) " + vformat(String(msg), ##__VA_ARGS__) + "[/color]")
+#endif
+#else
+#define DEBUG(msg, ...)
+#endif
+
+namespace godot {
+
+    class LogSystem : public RefCounted {
+            GDCLASS(LogSystem, RefCounted);
+
+        private:
+        public:
+            static const char *LOG_UPDATED_SIGNAL;
+
+            inline static LogSystem *get_instance() {
+                return dynamic_cast<LogSystem *>(godot::Engine::get_singleton()->get_singleton("LogSystem"));
+            }
+
+            enum LogLevel {
+                LOGLEVEL_DEBUG = 0,
+                LOGLEVEL_INFO,
+                LOGLEVEL_WARNING,
+                LOGLEVEL_ERROR,
+            };
+
+            LogSystem();
+            ~LogSystem() override = default;
+
+            void log(const LogLevel level, const String &line);
+            void debug(const String &line);
+            void info(const String &line);
+            void warning(const String &line);
+            void error(const String &line);
+
+        protected:
+            static void _bind_methods();
+    };
+} // namespace godot
+VARIANT_ENUM_CAST(LogSystem::LogLevel)
--- src/register_types.cpp
+++ src/register_types.cpp
@@ -6,6 +6,7 @@
 
 #include "brakes/TrainBrake.hpp"
 #include "core/GenericTrainPart.hpp"
+#include "core/LogSystem.hpp"
 #include "core/TrainController.hpp"
 #include "core/TrainPart.hpp"
 #include "core/TrainSystem.hpp"
@@ -20,6 +21,7 @@
 using namespace godot;
 
 TrainSystem *train_system_singleton = nullptr;
+LogSystem *log_system_singleton = nullptr;
 
 void initialize_libmaszyna_module(ModuleInitializationLevel p_level) {
     if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
@@ -39,9 +41,12 @@ void initialize_libmaszyna_module(ModuleInitializationLevel p_level) {
         GDREGISTER_CLASS(TrainController);
         GDREGISTER_CLASS(TrainSecuritySystem);
         GDREGISTER_CLASS(TrainSystem);
+        GDREGISTER_CLASS(LogSystem);
 
         train_system_singleton = memnew(TrainSystem);
+        log_system_singleton = memnew(LogSystem);
         Engine::get_singleton()->register_singleton("TrainSystem", train_system_singleton);
+        Engine::get_singleton()->register_singleton("LogSystem", log_system_singleton);
     }
 }
 
@@ -52,6 +57,7 @@ void uninitialize_libmaszyna_module(ModuleInitializationLevel p_level) {
 
     if (train_system_singleton) {
         Engine::get_singleton()->unregister_singleton("TrainSystem");
+        Engine::get_singleton()->unregister_singleton("LogSystem");
         // memdelete(train_system_singleton);
         // train_system_singleton = nullptr;
     }
